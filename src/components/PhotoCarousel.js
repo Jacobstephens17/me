1cf315c438ac5a73b6a03c4c1cb4111faa6da607
Jacob Stephens <jacobstephens2@gmail.com>
2021-01-28 11:31:02 -0800

    removed unused items

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -1,37 +1,11 @@
-import axios from 'axios'
-import React, { useState } from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+import React from 'react';
 import '../App.css'
 
 const PhotoCarousel = () => {
-    const [galleryImages, setGalleryImages] = useState();
 
-    const getImages = () => {
-
-        axios 
-            .get('https://api.unsplash.com/photos/random/?client_id=3CUtCSs0mCAULF3oD1B4jp4tb5MtBk3MOSsK-TZZ1_Q')
-            .then((res) => {
-                console.log(res)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-    }
-
-
+   
     return (
         <div>
-        <AliceCarousel
-        items={galleryImages}
-        autoPlayInterval={2000}
-        autoPlayDirection="rtl"
-        autoPlay={true}
-        fadeOutAnimation={true}
-        mouseTrackingEnabled={true}
-        disableAutoPlayOnAction={true}
-      />
       </div> 
     )
 }
